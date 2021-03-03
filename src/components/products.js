eb7b19ba0aee599853028571b5ad6be773cd41c2
galigopi <galigopigopi@gmail.com>
2021-03-03 13:17:17 +0530

    product

--- src/components/products.js
+++ src/components/products.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react'
+import formatCurrency from "../utils";
+
 
 export default class products extends Component {
     render() {
         return (
             <div>
                 <ul className="products">
-                    {this.props.products.map((product)=>(
+                    {this.props.products.map((product) => (
                         <li key={product._id}>
                             <div className="product">
-                                <a href={"#"+product._id}>
-                                    <img src={product.img} alt={product.title}></img>
+                                <a href={"#" + product._id}>
+                                    <img src={product.image} alt={product.title}></img>
                                     <p>
                                         {product.title}
                                     </p>
                                 </a>
                                 <div className="product-price">
                                     <div>
-                                        {product.price}
+                                        {formatCurrency(product.price)}
                                     </div>
                                     <button className="button primary">
                                         Add to Cart
